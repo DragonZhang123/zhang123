e328b183f5fba56df94f3ec9d6b1932acf855f88
zhang <zgl241@126.com>
2018-02-02 14:38:20 +0800

    add demo frame

---
+++ src/main/java/Game1/TT.java
@@ -0,0 +1,16 @@
+package Game1;
+
+import javax.swing.*;
+
+/**
+ * Created by zhang on 2018/2/1.
+ */
+public class TT extends JFrame {
+    private TetrisPanel tp;
+
+    static {
+    }
+
+    private class TetrisPanel extends JPanel {
+    }
+}
